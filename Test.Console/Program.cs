c34517b42fe4bb56993fca44cee6ec9e298622a3
Gökhan Menteşe <gkhnmnts@gmail.com>
2022-04-05 13:35:25 +0300

    Add project files.

---
+++ Test.Console/Program.cs
@@ -0,0 +1,21 @@
+﻿using CrmService.CrmHelper;
+using Microsoft.Crm.Sdk.Messages;
+
+namespace Test.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var crmServiceSingleton = CrmServiceSingleton.GetService();
+            var crmService = crmServiceSingleton.OrganizationService;
+
+            WhoAmIRequest request = new WhoAmIRequest();
+            WhoAmIResponse response = (WhoAmIResponse)
+            crmService.Execute(request);
+
+            var userId = response.UserId;
+
+        }
+    }
+}
